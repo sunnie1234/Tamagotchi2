01e0d8dc88a158dde424ef22402eb013af460d25
sunnie1234 <sunnie.pallante@elev.ga.ntig.se>
2023-10-12 14:33:28 +0200

    klar tror jag

--- Tamagotchi2/Program.cs
+++ Tamagotchi2/Program.cs
@@ -1,5 +1,45 @@
-﻿Tamagotchi tama = new();
+﻿
+Tamagotchi tama = new();
 
-tama.PrintStats();
+
+Console.WriteLine("Döp ditt husdjur");
+
+
+tama.Name = Console.ReadLine();
+
+while (tama.GetAlive()==true)
+{
+   
+   Console.WriteLine($"Tryck 1 för att mata, 2 för att anropa ord, 3 för att lära {tama.Name} ett nytt ord!");
+   tama.PrintStats();
+
+   string svar = Console.ReadLine();
+
+   if (svar =="1")
+   {
+      tama.Feed();
+      tama.Tick();
+   }
+   else if (svar == "2")
+   {
+      tama.Hi();
+      tama.Tick();
+   }
+   else if (svar == "3")
+   {
+      tama.Teach(Console.ReadLine());
+      tama.Tick();
+   }
+   else
+   {
+      Console.WriteLine("Fel input");
+      tama.Tick();
+   }
+
+   
+}
+
+Console.WriteLine("den död");
+
 
 Console.ReadLine();
--- Tamagotchi2/Tamagotchi.cs
+++ Tamagotchi2/Tamagotchi.cs
@@ -4,7 +4,7 @@ public class Tamagotchi
 {
     int Hunger = 0;
     int Boredom = 0;
-    List<string> words = new() {"iii"};
+    List<string> words = new() {""};
     bool isAlive = true;
     Random generator = new Random();
     public string Name;
@@ -12,27 +12,49 @@ public class Tamagotchi
 
     public void PrintStats()
     {
-        Console.WriteLine($"Hunger = {Hunger}, Boredom = {Boredom} Vocabulary: {words.Count} words");
+        Console.WriteLine($"{Name}\nHunger = {Hunger}, Boredom = {Boredom} Vocabulary: {words.Count} words");
     }
 
     public void Feed()
     {
-
+        Hunger-=2;
+        if (Hunger < 0)
+        {
+            Hunger = -1;
+        }
     }
 
     public void Hi()
     {
-
+        int wordNum = generator.Next(words.Count);
+        Console.WriteLine(words[wordNum]);
+        ReduceBoredom();
     }
 
     public void Teach(string word)
     {
-
+        words.Add(word);
+        ReduceBoredom();
     }
 
     public void Tick()
     {
-        
+        Boredom += 1;
+        Hunger += 1;
+        if (Boredom >= 10 || Hunger >= 10)
+        {
+            isAlive = false;
+        }
+    }
+
+    public bool GetAlive()
+    {
+        return isAlive;
+    }
+
+    private void ReduceBoredom()
+    {        
+        Boredom--;
     }
 
 
